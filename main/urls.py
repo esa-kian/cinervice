f60273c127e63269ac8cbf88bab08e9bd217cec6
Ehsan Ahmadi Kian <ehsan_amd72@hotmail.com>
2020-05-28 02:48:22 +0430

    Create Base.html template

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('movies/', views.movies, name='movies'),
     path('tv_shows/', views.series, name='series'),
-    path('peoples/', views.peoples, name='peoples'),
-    path('movie_theater', views.cinemas, name='cinemas'),
+    path('people/', views.peoples, name='peoples'),
+    path('movie_theaters/', views.cinemas, name='cinemas'),
 ]
