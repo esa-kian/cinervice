fb5cf479a651e07ed6d72778f405e48b56b168a9
Ehsan Ahmadi Kian <ehsan_amd72@hotmail.com>
2020-06-12 20:16:43 +0430

    Added redirect after rating

--- main/views.py
+++ main/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .models import Movie, Series, People, Cinema, Role
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+# Start of Movie Part Methods
 
 def movies(request):
     movies_list = Movie.objects.order_by('point')
@@ -27,12 +29,16 @@ def movie_rate(request, movie_id):
             movie_info = Movie.objects.get(pk=movie_id)
             rate = movie_info.point
             rate = ((rate * movie_info.count) + int(vote)) / (movie_info.count + 1)
-            return HttpResponse(Movie.objects.filter(id=movie_id).update(point=rate, count=movie_info.count + 1))
+            Movie.objects.filter(id=movie_id).update(point=rate, count=movie_info.count + 1)
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
         else:
             return HttpResponse('Your vote is out of range')
     else: 
         return HttpResponse('Invalid request')
 
+# End of Movie Part Methods
+
+# Start of TV Shows Part Methods
 
 def series(request):
     series_list = Series.objects.order_by('point')
@@ -55,11 +61,16 @@ def seri_rate(request, seri_id):
             seri_info = Series.objects.get(pk=seri_id)
             rate = seri_info.point
             rate = ((rate * seri_info.count) + int(vote)) / (seri_info.count + 1)
-            return HttpResponse(Series.objects.filter(id=seri_id).update(point=rate, count=seri_info.count + 1))
+            Series.objects.filter(id=seri_id).update(point=rate, count=seri_info.count + 1)
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
         else:
             return HttpResponse('Your vote is out of range')
     else: 
         return HttpResponse('Invalid request')
+
+# End of TV Shows Part Methods
+
+# Start of Celebrity Part Methods
 
 def peoples(request):
     people_list = People.objects.order_by('point')
@@ -89,11 +100,16 @@ def people_rate(request, people_id):
             people_info = People.objects.get(pk=people_id)
             rate = people_info.point
             rate = ((rate * people_info.count) + int(vote)) / (people_info.count + 1)
-            return HttpResponse(People.objects.filter(id=people_id).update(point=rate, count=people_info.count + 1))
+            People.objects.filter(id=people_id).update(point=rate, count=people_info.count + 1)
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
         else:
             return HttpResponse('Your vote is out of range')
     else: 
         return HttpResponse('Invalid request')
+
+# End of Celebrity Part Methods
+
+# Start of Movie Theater Part Methods
 
 def cinemas(request):
     cinema_list = Cinema.objects.order_by('point')
@@ -116,14 +132,18 @@ def cinema_rate(request, cinema_id):
             cinema_info = Cinema.objects.get(pk=cinema_id)
             rate = cinema_info.point
             rate = ((rate * cinema_info.count) + int(vote)) / (cinema_info.count + 1)
-            return HttpResponse(Cinema.objects.filter(id=cinema_id).update(point=rate, count=cinema_info.count + 1))
+            Cinema.objects.filter(id=cinema_id).update(point=rate, count=cinema_info.count + 1)
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
         else:
             return HttpResponse('Your vote is out of range')
     else: 
         return HttpResponse('Invalid request')
+# End of Movie Theater Part Methods
 
+# About Part Method
 def about(request):
     return render(request, 'about.html')
 
+# Contact Part Method
 def contact(request):
     return render(request, 'contact.html')
