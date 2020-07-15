2c68351f9e70b6913ebbbcc2aa0b20aea93cadee
Ehsan Ahmadi Kian <ehsan_amd72@hotmail.com>
2020-07-15 20:29:00 +0430

    fix bug in collectstatic command

--- cinervice/settings.py
+++ cinervice/settings.py
@@ -145,10 +145,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
 ]
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #redirect URL upon successful Google login
 
 SITE_ID = 1
