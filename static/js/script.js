45abd46dea65bf54e577101cac306dc30c017b9d
Ehsan Ahmadi Kian <ehsan_amd72@hotmail.com>
2020-07-19 16:53:52 +0430

    about

--- static/js/script.js
+++ static/js/script.js
@@ -1,21 +1,21 @@
 
-// function myFunction() {
-//   var x = document.getElementById("myTopnav");
-//   if (x.className === "topnav") {
-//     x.className += " responsive";
-//   } else {
-//     x.className = "topnav";
-//   }
-// }
-// var btnContainer = document.getElementById("myTopnav");
-// var btns = btnContainer.getElementsByClassName("btn");
-// for (var i = 0; i < btns.length; i++) {
-// btns[i].addEventListener("click", function() {
-// var current = document.getElementsByClassName("active");
-// current[0].className = current[0].className.replace(" active", "");
-// this.className += " active";
-// });
-// } 
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+var btnContainer = document.getElementById("myTopnav");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+btns[i].addEventListener("click", function() {
+var current = document.getElementsByClassName("active");
+current[0].className = current[0].className.replace(" active", "");
+this.className += " active";
+});
+} 
 
 // index script
 var slideIndex = 1;
