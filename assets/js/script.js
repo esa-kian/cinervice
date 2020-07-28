e20a8ff3b8a3cdfea3e97438bb91dd22ddf43934
Ehsan Ahmadi Kian <ehsan_amd72@hotmail.com>
2020-07-28 19:09:04 +0430

    rating

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,65 @@
 $(document).ready(function() {
   $(".dropdown-toggle").dropdown();
 });
-// Active nav
-$(document).ready(function() {
-  $.each($('#navbar').find('li'), function() {
-      $(this).toggleClass('active', 
-          window.location.pathname.indexOf($(this).find('a').attr('href')) > -1);
-  }); 
+var header = document.getElementById("navbar");
+var btns = header.getElementsByClassName("nav-item");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
+var $stars;
+
+jQuery(document).ready(function ($) {
+
+  // Custom whitelist to allow for using HTML tags in popover content
+  var myDefaultWhiteList = $.fn.tooltip.Constructor.Default.whiteList
+  myDefaultWhiteList.textarea = [];
+  myDefaultWhiteList.button = [];
+
+  $stars = $('.rate-popover');
+  
+  $stars.on('mouseover', function () {
+    var index = $(this).attr('data-index');
+    markStarsAsActive(index);
+  });
+  
+  function markStarsAsActive(index) {
+    unmarkActive();
+    for (var i = 0; i <= index; i++) {
+      $($stars.get(i)).addClass('amber-text');
+    }
+  }
+
+  function unmarkActive() {
+    $stars.removeClass('amber-text');
+  }
+  
+  $stars.on('click', function () {
+    $stars.popover('hide');
+  });
+
+  // Submit, you can add some extra custom code here
+  // ex. to send the information to the server
+  $('#rateMe').on('click', '#voteSubmitButton',  function() {
+    $stars.popover('hide');
+  });
+
+  // Cancel, just close the popover
+  $('#rateMe').on('click', '#closePopoverButton', function() {
+    $stars.popover('hide');
+  });
+
+});
+
+$(function () {
+  $('.rate-popover').popover({
+    // Append popover to #rateMe to allow handling form inside the popover
+    container: '#rateMe',
+    // Custom content for popover
+    content: `<div class="my-0 py-0"> <textarea type="text" style="font-size: 0.78rem" class="md-textarea form-control py-0" placeholder="Write us what can we improve" rows="3"></textarea> <button id="voteSubmitButton" type="submit" class="btn btn-sm btn-primary">Submit!</button> <button id="closePopoverButton" class="btn btn-flat btn-sm">Close</button>  </div>`
+  });
+  $('.rate-popover').tooltip();
 });
