5b818c72be07de0f2003d1e66ba947092d49acca
Ehsan Ahmadi Kian <ehsan_amd72@hotmail.com>
2020-08-03 01:13:30 +0430

    location

--- static/js/script.js
+++ static/js/script.js
@@ -2,12 +2,5 @@
 $(document).ready(function() {
   $(".dropdown-toggle").dropdown();
 });
-var header = document.getElementById("navbar");
-var btns = header.getElementsByClassName("nav-item");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
-  });
-}
+// active class 
+
